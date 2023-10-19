fcf77b42aaf91e0ef2bb4f891a572b9cee9b1eab
Javier Placencia <javix.plax@gmail.com>
2023-10-19 20:04:49 -0300

    Verificacion de plazos de apelacion y aplicacion del middleware

--- backend/src/services/postulacion.service.js
+++ backend/src/services/postulacion.service.js
@@ -1,6 +1,7 @@
 "use strict";
 // Importa el modelo de datos 'User'
 const Postula = require("../models/postula.model.js");
+const Beca = require("../models/beca.model.js");
 const { handleError } = require("../utils/errorHandler");
 
 /**
@@ -34,6 +35,19 @@ async function createApelacion(archivos, id) {
     //Buscar al postulante
     const postulacionFound = await Postula.findOne({ postulante: id })
     if (!postulacionFound) return [null, "El usuario no tiene postulacion"];
+
+    //Verificacion de plazos (maximo 2 semanas despues de la fecha de fin de la beca)
+    const fecha_actual = Date.now();
+    const beca = await Beca.findById(postulacionFound.beca);
+    if (!beca) return [null, "No se encontró la beca"];
+    const fechaFinalizacionApelacion = new Date(beca.fecha_fin); //Se crea una fecha con la fecha de fin de la beca
+    fechaFinalizacionApelacion.setDate(fechaFinalizacionApelacion.getDate() + 14); //Se le suman 14 dias a la fecha de fin de la beca
+    if (fecha_actual > fechaFinalizacionApelacion) { //Si se sobre pasa de las 2 semanas de plazo siguientes a la fecha fin de la beca
+      return [null, "El plazo de apelacion ha vencido"];
+    }
+    if (fecha_actual < beca.fecha_fin){ //Si aun no termina el periodo de postulacion la beca
+      return [null, "El periodo de apelacion aun no comienza"];
+    }
     
     //Verificacion de postulacion previa rechazada
     if (postulacionFound.estado == "Apelada") return [null, "El usuario ya tiene una apelacion en proceso"];
