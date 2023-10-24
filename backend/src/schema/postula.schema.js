ed9808de08d35d180d5f923fe0e4d11e31ba09b2
joigs <jose.saez2001@alumnos.ubiobio.cl>
2023-10-24 01:09:01 -0300

    schema provisional

--- backend/src/schema/postula.schema.js
+++ backend/src/schema/postula.schema.js
@@ -2,5 +2,15 @@
 
 const Joi = require("joi");
 
-
-module.exports = {  };
+const postulaPuntajeSchema = Joi.object({
+    puntaje: Joi.number()
+      .integer()
+      .required()
+      //.pattern(/^[0-9]+$/)
+      .messages({
+        "string.base": "El puntaje debe ser de tipo int.",
+        //"string.pattern.base": "El puntaje proporcionado no es un numero",
+      }),
+  });
+  
+module.exports = { postulaPuntajeSchema };
