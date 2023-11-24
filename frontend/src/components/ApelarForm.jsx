430faa04a754ffaa09bf30d18806b5bc14b91c41
Javier Placencia <javix.plax@gmail.com>
2023-11-24 00:56:24 -0300

    Formulario de apelacion con subida de documentos

--- frontend/src/components/ApelarForm.jsx
+++ frontend/src/components/ApelarForm.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { apelar } from '../services/apelacion.service';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getEstado } from "../services/estado.service";
 
 function ApelarForm() {
   const navigate = useNavigate();
-  const [error, setError] = useState(null);
+  const [errorApelacion, setErrorApelacion] = useState(null);
 
   const {
     register,
@@ -15,31 +16,86 @@ function ApelarForm() {
 
   const onSubmit = (data) => {
     // Aquí puedes manejar la subida del archivo
-    const file = data.archivoPDF[0];
-    apelar(file).then((response) => {
+    const motivo = data.motivo;
+    const files = Object.keys(data)
+      .filter(key => key.startsWith('archivoPDF'))
+      .flatMap(key => Array.from(data[key]));
+
+    if (!files) {
+      setErrorApelacion('Para apelar se deben subir los documentos faltantes');
+      return;
+    }
+    if (!motivo) {
+      setErrorApelacion('Para apelar se debe ingresar el motivo de la apelación');
+      return;
+    }
+    apelar(motivo, files).then((response) => {
       if (response.state === 'Success') {
         navigate('/postulacion/estado', { state: { success: response.data } });
       } else {
-        setError(response.message);
+        setErrorApelacion(response.message);
       }
     });
   };
 
+  //Obtencion de documentosFaltantes
+  const [postulacion, setPostulacion] = useState(null);
+  const [apelacion, setApelacion] = useState(null);
+  const [errorEstado, setErrorEstado] = useState(null);
+  useEffect(() => {
+    getEstado().then((response) => {
+      if(response.state === "Success") {
+        setPostulacion(response.data.postulacion);
+        if (response.data.apelacion) { //En caso de que exista apelacion
+          setApelacion(response.data.apelacion);
+        }
+      }else{
+        console.log(response.message);
+        setErrorEstado(response.message);
+      }
+    });
+  }, []);
+
   return (
     <>
-      {error && <div className="error-banner">{error}</div>}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2>Formulario de Apelación</h2>
-        <label className="input-label" htmlFor="archivoPDF"><strong>Subir documentos</strong></label>
-        <input
-          id="archivoPDF"
-          name="archivoPDF"
-          type="file"
-          {...register('archivoPDF', { required: true })}
-        />
-        {errors.archivoPDF && <span>*Por favor, sube tus documentos</span>}
-        <input type="submit" />
-      </form>
+      {errorApelacion && <div className="error-banner">{errorApelacion}</div>}
+        {postulacion ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Formulario de Apelación</h2>
+          <p><strong>Beca a apelar:</strong> {postulacion.beca.nombre}</p>
+          <label className="input-label" htmlFor="motivo"><strong>Motivo de la apelación</strong></label>
+          <input
+            id="motivo"
+            name="motivo"
+            type="text"
+            {...register('motivo', { required: true })}
+          />
+          {errors.motivo && <span className='campo-obligatorio'>*Por favor, ingresa el motivo de la apelación</span>}
+          <label><strong>Subir documentos</strong></label>
+          {postulacion.documentosFaltantes && postulacion.documentosFaltantes.length > 0 ? (
+            postulacion.documentosFaltantes.map((documento, index) => (
+              <div key={index}>
+                <label className="label-file" htmlFor={`archivoPDF${index}`}>
+                  {documento}
+                  <input
+                    id={`archivoPDF${index}`}
+                    name={`archivoPDF${index}`}
+                    type="file"
+                    className='input-file'
+                    {...register(`archivoPDF${index}`, { required: true })}
+                  />
+                </label>
+                {errors[`archivoPDF${index}`] && <span className='campo-obligatorio'>*Por favor, sube tus documentos</span>}
+              </div>
+            ))
+          ) : (
+            <p>No hay documentos faltantes para subir.</p>
+          )}
+          <input type="submit" />
+        </form>
+        ) : (
+          <div>{errorEstado && <div className="error-banner">{errorEstado}</div>}</div>
+        )}
     </>
   );
 }
