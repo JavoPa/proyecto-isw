58fd011049172ad5e4af3d0ffa08c6059913b356
Javier Placencia <javix.plax@gmail.com>
2023-11-24 00:57:18 -0300

    Se manejan errores y botones de estado

--- frontend/src/routes/Estado.jsx
+++ frontend/src/routes/Estado.jsx
@@ -1,40 +1,58 @@
 import { useState, useEffect } from "react";
 import { getEstado } from "../services/estado.service";
+import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Estado() {
   const [postulacion, setPostulacion] = useState(null);
   const [apelacion, setApelacion] = useState(null);
+  const location = useLocation(); // Para obtener el estado de la solicitud
+  const success = location.state?.success;
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    getEstado().then((data) => {
-      setPostulacion(data.postulacion);
-      if (data.apelacion) {
-        setApelacion(data.apelacion);
+    getEstado().then((response) => {
+      if(response.state === "Success") {
+        setPostulacion(response.data.postulacion);
+        if (response.data.apelacion) { //En caso de que exista apelacion
+          setApelacion(response.data.apelacion);
+        }
+      }else{
+        setError(response.message);
       }
     });
   }, []);
 
   return (
-    <div className="estado-container">
-      <h2>Estado de la postulación</h2>
-      {postulacion ? (
-        <div className="estado-details">
-          <p><strong>Estado:</strong> {postulacion.estado}</p>
-          <p><strong>Motivos:</strong> {postulacion.motivos}</p>
-          <p><strong>Beca:</strong> {postulacion.beca.nombre}</p>
-          <p><strong>Fecha de postulación:</strong> {postulacion.fecha_de_recepcion}</p>
-          {apelacion && (
-            <div className="estado-apelacion">
-              <h2>Estado de la apelación</h2>
-              <p><strong>Estado:</strong> {apelacion.estado}</p>
-              <p><strong>Fecha de apelación:</strong> {apelacion.fecha_de_apelacion}</p>
-            </div>
-          )}
-        </div>
-      ) : (
-        <div>Cargando...</div>
-      )}
-    </div>
+    <>
+      {success && <div className="success-banner">{success}</div>}
+      <div className="estado-container">
+        <h2>Estado de la postulación</h2>
+        {postulacion ? (
+          <div className="estado-details">
+            <p><strong>Estado:</strong> {postulacion.estado}</p>
+            <p><strong>Motivos:</strong> {postulacion.motivos}</p>
+            <p><strong>Beca:</strong> {postulacion.beca.nombre}</p>
+            <p><strong>Fecha de postulación:</strong> {postulacion.fecha_de_recepcion}</p>
+            {postulacion.estado==='Rechazada' && !apelacion && (
+              <Link to="/postulacion/apelar">
+                <button type="button">Apelar</button>
+              </Link>
+            )}
+            {apelacion && (
+              <div className="estado-apelacion">
+                <h2>Estado de la apelación</h2>
+                <p><strong>Estado:</strong> {apelacion.estado}</p>
+                <p><strong>Motivos:</strong> {apelacion.motivos}</p>
+                <p><strong>Fecha de apelación:</strong> {apelacion.fecha_de_apelacion}</p>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>{error && <div className="error-banner">{error}</div>}</div>
+        )}
+      </div>
+    </>
   );
 }
 
