74a2b758c17699989b5e2e8ae6c053ce1ab35944
joigs <jose.saez2001@alumnos.ubiobio.cl>
2023-12-17 04:39:03 -0300

    commit para pulear

--- frontend/src/components/PostularForm.jsx
+++ frontend/src/components/PostularForm.jsx
@@ -1,40 +1,17 @@
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { createPostulacion, getMyUser } from '../services/postulacion.service';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { createPostulacion, getMyUser } from "../services/postulacion.service"; // Import the service functions
+import { useForm } from "react-hook-form";
 
 
-function PostularForm({ becaId }) {
-  const navigate = useNavigate();
-  const [errorPostulacion, setErrorPostulacion] = useState(null);
+const PostularForm = ({ becaId }) => {
+    
+  const [comment, setComment] = useState("");
   const [userData, setUserData] = useState(null);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  
+  const { register, handleSubmit, setError, formState: { errors } } = useForm(); // Utiliza useForm de react-hook-form
 
-  const onSubmit = (data) => {
-    const comentario = data.comentario;
-    const files = Object.keys(data)
-      .filter(key => key.startsWith('archivoPDF'))
-      .flatMap(key => Array.from(data[key]));
-
-    if (!files) {
-      setErrorPostulacion('Para apelar se deben subir los documentos faltantes');
-      return;
-    }
-    if (!comentario) {
-      comentario = "";
-    }
-    createPostulacion(comentario, files).then((response) => {
-      if (response.state === 'Success') {
-        navigate('/postulacion/becas', { state: { success: response.data } });
-      } else {
-        setErrorPostulacion(response.message);
-      }
-    });
-  };
+  
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -49,41 +26,256 @@ function PostularForm({ becaId }) {
     fetchUserData();
   }, []);
 
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    const selected = Array.from(files).slice(0, 5); // Limit to 5 files
+    setSelectedFiles(selected);
+  };
+
+  const handlePostulacion = async (data) => {
+    const comentario = data.comment;
+
+    if (selectedFiles.length === 0) {
+        setError("files", {
+          type: "manual",
+          message: "Debe subir al menos un archivo",
+        });
+        return;
+      }
+      const formData = new FormData();
+  formData.append("comentario", comentario);
+
+  selectedFiles.forEach((file, index) => {
+    formData.append(`file${index}`, file);
+  });
+
+
+  try {
+    const response = await createPostulacion(formData, becaId);
+    if (response && response.status === 201) {
+      console.log("Postulacion creada correctamente");
+    } else {
+        console.error("Error en la postulación:", error.response || error);
+        console.log(formData);
+        console.log(becaId);
+
+    }
+  } catch (error) {
+    console.error("Error en la postulación:", error);
+  }
+};
   return (
-    <>
-      
-        {userData ? (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          
-          <label className="input-label" htmlFor="comentario"><strong>comentario de la postulacion</strong></label>
-          <input
-            id="comentario"
-            name="comentario"
-            type="text"
-            {...register('comentario')}
-          />
-          <label><strong>Subir documentos</strong></label>
-          {userData ?  (
-            
-              <div key={index}>
-                <label className="label-file" htmlFor={`archivoPDF${index}`}>
-                  {documento}
-                  <input
-                    id={`archivoPDF${index}`}
-                    name={`archivoPDF${index}`}
-                    type="file"
-                    className='input-file'
-                    {...register(`archivoPDF${index}`, { required: true })}
-                  />
-                </label>
-              </div>
-            
-          ) : (true)}
-          <input type="submit" />
+    <div>
+      {userData ? (
+        <form onSubmit={handleSubmit(handlePostulacion)} style={{ display: "flex" }}>
+        <div className="container flex-container">
+          <div className="form-group">
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Nombres</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.nombres}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Rut</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.rut}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Direccion</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.direccion}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Discapacidad</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={
+                  userData.data.discapacidad
+                    ? "Presenta discapacidad"
+                    : "No presenta "
+                }
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Fecha de nacimiento</label>
+              {userData.data.fecha_nacimiento && (
+                <input
+                  type="text"
+                  style={{
+                    display: "flex",
+                    marginBottom: "10px",
+                    backgroundColor: "#9E9E9E",
+                  }}
+                  className="form-control"
+                  value={new Date(
+                    userData.data.fecha_nacimiento
+                  ).toLocaleDateString("es-ES")}
+                  disabled
+                />
+              )}
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Apellidos</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.apellidos}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Sexo</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.sexo}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Telefono</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.telefono}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Correo electronico</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.email}
+                disabled
+              />
+            </div>
+            <div
+              style={{ justifyContent: "space-between", marginBottom: "10px" }}
+            >
+              <label>Cuenta bancaria</label>
+              <input
+                type="text"
+                style={{
+                  display: "flex",
+                  marginBottom: "10px",
+                  backgroundColor: "#9E9E9E",
+                }}
+                className="form-control"
+                value={userData.data.cuenta_bancaria}
+                disabled
+              />
+            </div>
+          </div>
+          </div>
+
+
+
+          <div className="form-group">
+            <label>Comentario</label>
+            <textarea
+              className="form-control"
+              value={comment}
+              onChange={handleCommentChange}
+              placeholder="Si lo desea, puede agregar un comentario a su postulación."
+            />
+          </div><div className="form-group">
+            <label>Subir archivos (máximo 5)</label>
+            <input
+              type="file"
+              className="form-control-file"
+              multiple  // Allow multiple file selection
+              onChange={handleFileChange} // Handle file selection
+              accept=".pdf,.jpg,.jpeg,.png" // Define accepted file types if necessary
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Enviar
+          </button>
         </form>
-        ) : (true)}
-    </>
+      ) : (
+        <p>Loading user data...</p>
+      )}
+    </div>
   );
-}
+};
 
 export default PostularForm;
