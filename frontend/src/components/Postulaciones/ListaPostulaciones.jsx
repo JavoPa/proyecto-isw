9a47a1c8fbb1bee1afc0f0865765836be96ba5e7
Tomas Sepulveda <tomas.sepulveda2001@alumnos.ubiobio.cl>
2023-12-17 04:07:42 -0300

    se añade barra de busqueda para postulaciones y postulantes

--- frontend/src/components/Postulaciones/ListaPostulaciones.jsx
+++ frontend/src/components/Postulaciones/ListaPostulaciones.jsx
@@ -1,4 +1,3 @@
-// ListaPostulaciones.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getPostulaciones, getInforme } from '../../services/estado.service';
@@ -6,20 +5,57 @@ import { getPostulaciones, getInforme } from '../../services/estado.service';
 const ListaPostulaciones = () => {
     const [Postulaciones, setPostulaciones] = useState([]);
     const [Informe, setInforme] = useState(null);
+    const [busqueda, setBusqueda] = useState('');
+    const [busquedaType, setBusquedaType] = useState('estado'); // Default search type
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const cargarPostulaciones = async () => {
-            try {
-                const PostulacionesData = await getPostulaciones();
-                setPostulaciones(PostulacionesData);
-            } catch (error) {
-                console.error('Error al obtener los Postulaciones:', error);
+    const cargarPostulaciones = async () => {
+        try {
+            const Postulaciones = await getPostulaciones();
+            let data = [];
+            for (const i in Postulaciones.data) {
+                data.push({
+                    _id: Postulaciones.data[i]._id,
+                    fechaRecepcion: Postulaciones.data[i].fecha_recepcion,
+                    estado: Postulaciones.data[i].estado,
+                    motivos: Postulaciones.data[i].motivos,
+                    nombreBeca: Postulaciones.data[i].beca.nombre,
+                    nombrePostulante: Postulaciones.data[i].postulante.nombres + ' ' + Postulaciones.data[i].postulante.apellidos,
+                    puntaje: parseInt(Postulaciones.data[i].puntaje),
+                    documentosFaltantes: Postulaciones.data[i].documentosFaltantes
+                });
             }
-        };
 
+            if (busqueda) {
+                console.log(busqueda, busquedaType);
+    
+                switch (busquedaType) {
+                    case 'puntaje':
+                        data = data.filter((postulacion) =>
+                            postulacion[busquedaType] === parseInt(busqueda)
+                        );
+                        break;
+                    default:
+                        data = data.filter((postulacion) =>
+                            postulacion[busquedaType].toLowerCase().includes(busqueda.toLowerCase())
+                        );
+                        break;
+                }
+            }
+            
+            setPostulaciones(data);
+        } catch (error) {
+            console.error('Error al obtener los Postulaciones:', error);
+        }
+    };
+
+    useEffect(() => {
         cargarPostulaciones();
-    }, []);
+    }, [])
+
+    const handleBuscar = () => {
+        cargarPostulaciones();
+    };
 
     useEffect(() => {
         const getInformeGeneral = async () => {
@@ -35,48 +71,57 @@ const ListaPostulaciones = () => {
         getInformeGeneral();
     }, [])
 
+    const handleBuscarTypeChange = (e) => {
+        setBusquedaType(e.target.value);
+    };
+
     const handleInforme = (event) => {
         if (Informe) {
-          event.preventDefault();
-          const a = document.createElement('a');
-          a.href = URL.createObjectURL(Informe);
-          a.download = 'InformeGeneral.pdf';
-          document.body.appendChild(a);
-          a.click();
-          document.body.removeChild(a);
-          URL.revokeObjectURL(a.href);
+            event.preventDefault();
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(Informe);
+            a.download = 'InformeGeneral.pdf';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(a.href);
         }
-      };
+    };
 
     const handleVerPostulacion = (id) => {
         navigate(`/gestion/postulacion/${id}`);
     };
 
-    let data = [];
-
-    for (const i in Postulaciones.data) {
-        data.push({
-            _id: Postulaciones.data[i]._id,
-            fechaRecepcion: Postulaciones.data[i].fecha_recepcion,
-            estado: Postulaciones.data[i].estado,
-            motivos: Postulaciones.data[i].motivos,
-            nombreBeca: Postulaciones.data[i].beca.nombre,
-            nombrePostulante: Postulaciones.data[i].postulante.nombres + ' ' + Postulaciones.data[i].postulante.apellidos,
-            puntaje: Postulaciones.data[i].puntaje,
-            documentosFaltantes: Postulaciones.data[i].documentosFaltantes
-        });
-    }
-
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
         const formattedDate = new Date(dateString).toLocaleDateString(undefined, options)
-          .split('/').join('-'); // Replace slashes with dashes
+            .split('/').join('-'); // Replace slashes with dashes
         return formattedDate;
-      };
+    };
 
     return (
         <form>
-            
+
+            <div className='filtro-container'>
+                <select
+                    value={busquedaType}
+                    onChange={handleBuscarTypeChange}
+                >
+                    <option value="estado">Estado</option>
+                    <option value="nombrePostulante">Postulante</option>
+                    <option value="puntaje">Puntaje</option>
+                </select>
+                <input
+                    type="text"
+                    id="busqueda"
+                    value={busqueda}
+                    onChange={(e) => setBusqueda(e.target.value)}
+                    placeholder='Busqueda'
+                />
+                <button type="button" onClick={handleBuscar}>
+                    Buscar
+                </button>
+            </div>
             <div>
                 <h1>Lista de Postulaciones</h1>
                 <table className="lista-apelaciones">
@@ -93,7 +138,7 @@ const ListaPostulaciones = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((postulacion) => (
+                        {Postulaciones.map((postulacion) => (
                             <tr key={postulacion._id} className='item-apelacion'>
                                 <td>{formatDate(postulacion.fechaRecepcion)}</td>
                                 <td>{postulacion.estado}</td>
@@ -120,4 +165,3 @@ const ListaPostulaciones = () => {
 };
 
 export default ListaPostulaciones;
-
