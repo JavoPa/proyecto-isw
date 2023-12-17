5387f024e7e41bced4e5a1539c4b15c0abfe2adc
Javier Placencia <javix.plax@gmail.com>
2023-12-17 02:05:17 -0300

    Ajuste pagina de inicio y user info

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,8 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <h1>Pagina principal</h1>
-    </>
+    <div className="App">
+      <header>
+        <h1>Portal de Becas Municipales</h1>
+      </header>
+      <main>
+        <h2>Bienvenido al portal de postulación a becas municipales</h2>
+        <p>Aquí puedes encontrar y postular a las becas disponibles en tu municipio.</p>
+        <Link className='home-button' to="/becas">Ver becas disponibles</Link>
+        <Link className='home-button' to="/postular">Postular a una beca</Link>
+      </main>
+      <footer>
+        <p>© 2023 - Municipalidad</p>
+      </footer>
+    </div>
   );
 }
 
