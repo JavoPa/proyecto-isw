5d1046a29e115e631ce6771104c546f08059b8f9
joigs <jose.saez2001@alumnos.ubiobio.cl>
2023-12-18 07:34:15 -0300

    palitos

--- frontend/src/services/postulacion.service.js
+++ frontend/src/services/postulacion.service.js
@@ -50,15 +50,3 @@ export async function createPostulacion(comentario, beca_id, files) {
     return error.response.data;
   }
 }
-
-export const registerUser = async (datas) => {
-  try {
-    const response = await axios.post('/auth/register', datas);
-    const { status, data } = response;
-    if (status === 201) {
-      return data;
-    }
-  } catch (error) {
-    return error.response.data;
-  }
-}
